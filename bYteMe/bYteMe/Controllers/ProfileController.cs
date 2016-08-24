4af700fbceddab4be6bd0d4662f5cc8c481fcc5d
Mirela Damyanova <mdamyanova181@gmail.com>
2016-08-24 14:56:28 +0300

    worked on profile page

--- bYteMe/bYteMe/Controllers/ProfileController.cs
+++ bYteMe/bYteMe/Controllers/ProfileController.cs
@@ -1,14 +1,31 @@
 ﻿namespace bYteMe.Controllers
 {
+    using System.Web;
     using System.Web.Mvc;
+
+    using bYteMe.Models;
+
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.Owin;
 
     [Authorize]
     public class ProfileController : Controller
     {
         public ActionResult Index()
         {
+            // get current user          
+            User user =
+                System.Web.HttpContext.Current.GetOwinContext()
+                    .GetUserManager<ApplicationUserManager>()
+                    .FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+           
             // TODO: Get current user and show info for him
-            return this.View();
+            return this.View(user);
         }
+
+        //public ActionResult ViewProfile()
+        //{
+        //    return this.View();
+        //}
     }
 }
